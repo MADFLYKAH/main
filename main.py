24f5c444cc1dc1c423e9e049d86d259e74178736
MADFLYKAH <kanbt@mail.ru>
2024-06-27 14:04:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# 1st program
+print((9**0.5)*5)
+
+# 2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+
+# 3rd program
+print((((1234 % 1000) // 10) + ((5678 % 1000) // 10)))
+
+# 4th program
+print(((int(13.42)) == (int(42.13 * 100) % 100)) or (int(42.13) == ((13.42 * 100) % 100)))
+
+# Старая версия задачи
+
+# Длинна слова
+a = "компьютер"
+print(len(a))
+
+# Суммы и разности
+first = 1234
+second = 5678
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+# Среднее арифметическое
+first = 1
+second = 2
+third = 3
+mean = (first + second + third) // 3
+print(mean)
+
+# Простые строчки
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string, ", ", first_string, sep="")
+
+# Сложная формула
+a = 2
+b = 3
+c = 4
+f = (((a*b) + (a*c)) ** 3) / 2
+print(f)
